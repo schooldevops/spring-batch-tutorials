87792e92f5e10eb5f6be58a226c755b78150cda9
kido <baekido@gmail.com>
2024-03-06 10:48:41 +0900

    add flatFileItemReader

---
+++ 06.01.FlatFileItemReaderSample/src/main/java/com/schooldevops/springbatch/batchsample/jobs/flatfilereader/FlatFileItemJobConfig.java
@@ -0,0 +1,81 @@
+package com.schooldevops.springbatch.batchsample.jobs.flatfilereader;
+
+
+import com.schooldevops.springbatch.batchsample.jobs.models.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+public class FlatFileItemJobConfig {
+
+    /**
+     * CHUNK 크기를 지정한다.
+     */
+    public static final int CHUNK_SIZE = 100;
+    public static final String ENCODING = "UTF-8";
+    public static final String FLAT_FILE_CHUNK_JOB = "FLAT_FILE_CHUNK_JOB";
+
+    @Bean
+    public FlatFileItemReader<Customer> flatFileItemReader() {
+
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("FlatFileItemReader")
+                .resource(new ClassPathResource("./customer.csv"))
+                .encoding(ENCODING)
+                .delimited().delimiter(",")
+                .names("name", "age", "gender")
+                .targetType(Customer.class)
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemWriter<Customer> flatFileItemWriter() {
+        return new FlatFileItemWriterBuilder<Customer>()
+                .name("flatFileItemWriter")
+                .resource(new FileSystemResource("./output/customer_new.csv"))
+                .encoding(ENCODING)
+                .delimited().delimiter("\t")
+                .names("Name", "Age", "Gender")
+                .build();
+    }
+
+
+    @Bean
+    public Step flatFileStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        log.info("------------------ Init flatFileStep -----------------");
+
+        return new StepBuilder("flatFileStep", jobRepository)
+                .<Customer, Customer>chunk(CHUNK_SIZE, transactionManager)
+                .reader(flatFileItemReader())
+                .writer(flatFileItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job flatFileJob(Step flatFileStep, JobRepository jobRepository) {
+        log.info("------------------ Init flatFileJob -----------------");
+        return new JobBuilder(FLAT_FILE_CHUNK_JOB, jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(flatFileStep)
+                .build();
+    }
+}
