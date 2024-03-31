22a6c3b6d5360a13f543793a841633a90df6c5ab
kido <baekido@gmail.com>
2024-03-31 17:18:45 +0900

    add mybatis paging item reader

---
+++ 06.06.MyBatisItemReaderSample/src/main/java/com/schooldevops/springbatch/batchsample/jobs/mybatis/CustomerItemProcessor.java
@@ -0,0 +1,14 @@
+package com.schooldevops.springbatch.batchsample.jobs.mybatis;
+
+import com.schooldevops.springbatch.batchsample.jobs.models.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemProcessor;
+
+@Slf4j
+public class CustomerItemProcessor implements ItemProcessor<Customer, Customer> {
+    @Override
+    public Customer process(Customer item) throws Exception {
+        log.info("Item Processor ------------------- {}", item);
+        return item;
+    }
+}
